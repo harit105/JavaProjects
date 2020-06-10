ba72550f95e4bdba8591e288bfbe321d27d1528b
harit105 <60238241+harit105@users.noreply.github.com>
2020-06-10 15:27:51 +0530

    Speed Converter

---
+++ speedConverter/src/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+
+      long miles=  speedConverter.toMilesPerHour(5);
+        System.out.println("Miles=" + miles);
+        speedConverter.printConversion(5);
+    }
+}
---
+++ speedConverter/src/speedConverter.java
@@ -0,0 +1,23 @@
+public class speedConverter {
+
+        public static long toMilesPerHour(double kilometersPerHour){
+
+            if(kilometersPerHour<0){
+                return -1;
+            }
+            long milesPerHour;
+            milesPerHour = Math.round(kilometersPerHour/1.609);
+            return milesPerHour;
+        }
+        public static void printConversion (double kilometersPerHour){
+
+            if(kilometersPerHour<0){
+                System.out.println("Invalid value");
+            }else{
+                long milesPerHour=toMilesPerHour(kilometersPerHour);
+                System.out.println(kilometersPerHour+ " km/h= "
+                + milesPerHour+ " mi/h");
+
+            }
+        }
+}
