f57831906bba753c8f009d9d6fb019974f25131d
harit105 <60238241+harit105@users.noreply.github.com>
2020-06-10 15:26:35 +0530

    Teen number checker

---
+++ TeenNumberChecker/src/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println(TeenNumberChecker.hasTeen(1,15,10));
+        System.out.println(TeenNumberChecker.isTeen(9));
+
+    }
+}
